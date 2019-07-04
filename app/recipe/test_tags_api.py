444f875b60a68b20d63427744da6c58b99abde1e
Alex Zaitsev <madeinussr91@gmail.com>
2019-07-04 16:38:36 +0300

    Added list tags endpoint

---
+++ app/recipe/test_tags_api.py
@@ -0,0 +1,61 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+
+from core.models import Tag
+from recipe.serializers import TagSerializer
+
+TAGS_URL = reverse('recipe:tag-list')
+
+
+class PublicTagsApiTests(APITestCase):
+    """Test the publicly available tags API"""
+
+    def test_login_required(self):
+        """Test that login is required for retrieving tags"""
+        response = self.client.get(TAGS_URL)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateTagsApiTest(APITestCase):
+    """Test the authorized user tags API"""
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            'test@test.com',
+            'testpass'
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_tags(self):
+        """Test retrieves tags"""
+        Tag.objects.create(user=self.user, name='Raw')
+        Tag.objects.create(user=self.user, name='Vegan')
+
+        response = self.client.get(TAGS_URL)
+
+        tags = Tag.objects.all().order_by('-name')
+        serializer = TagSerializer(tags, many=True)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_tags_limited_to_user(self):
+        """Test that tags returned are for the authenticated user"""
+        # create Tag for a new user
+        user2 = get_user_model().objects.create_user(
+            'other@test.com',
+            'testpass'
+        )
+        Tag.objects.create(user=user2, name='Fruity')
+        # create Tag for authorized user
+        tag_for_auth_user = Tag.objects.create(user=self.user,
+                                               name='Healthy food')
+        serializer = TagSerializer([tag_for_auth_user], many=True)
+
+        response = self.client.get(TAGS_URL)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
